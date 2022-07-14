d8ca5f17fe910db6d956581ba7c6153dc0d8d6ad
Guntae Kim <dkfkskgks@naver.com>
2022-07-14 13:59:45 +0900

    화면 오른쪽 상단의 오늘날짜 표시 구현

--- app/src/main/java/com/example/self_growup/MainActivity.java
+++ app/src/main/java/com/example/self_growup/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.self_growup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView TimeTextView;
+
+        TimeTextView = (TextView) findViewById(R.id.Today);
+        TimeTextView.setText((getTime()));
+
+
+
     }
+
+
+    private String getTime() {
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy년 MM월 dd일");
+        String getTime = dateFormat.format(date);
+        return getTime;
+    }
+
+
 }
